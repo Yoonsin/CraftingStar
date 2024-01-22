2bc77c2061edb21b23e0a8c2c85184227ae12346
Yoonshin Hwang <90170221+Yoonsin@users.noreply.github.com>
2024-01-22 15:40:13 +0900

    SteamSession Update
    1. 빌드 모듈 목록에 “UMGEditor” 가 있으면 빌드가 되지 않음. 문제는 팔레트 블루프린트 부모 클래스(c++)가 UBluePrintWidget인데 이것의 모듈이 UMGEditor.. 그래서 부모 클래스를 UUserWidget으로 바꾼다음, 모듈에서 UMGEditor를 지운 후 빌드하니 잘 패키징됨.
    
    2. 실험 결과 정상적으로 게임 세션이 작동.

--- Source/CraftingStar/CraftingStar.Build.cs
+++ Source/CraftingStar/CraftingStar.Build.cs
@@ -9,7 +9,7 @@ public class CraftingStar : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "UMGEditor", "Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG",  "Niagara" });
         PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
     }
 }
