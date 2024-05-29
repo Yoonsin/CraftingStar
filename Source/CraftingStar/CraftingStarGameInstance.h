f5ffe6c03b640a2efd9546517dc01ed7d5487c49
Yoonshin Hwang <90170221+Yoonsin@users.noreply.github.com>
2024-05-29 15:12:24 +0900

    Save Update

---
+++ Source/CraftingStar/CraftingStarGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AdvancedFriendsGameInstance.h"
+#include "CraftingStarGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRAFTINGSTAR_API UCraftingStarGameInstance : public UAdvancedFriendsGameInstance
+{
+	GENERATED_BODY()
+	
+};
